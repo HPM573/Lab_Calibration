b923acd593e2f8e80630d0a1b9351ab2953c7380
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-03-04 19:46:31 -0500

    comparing calibrated with uncalibrated model

--- RunCalibratedModel.py
+++ RunCalibratedModel.py
@@ -1,14 +1,23 @@
 import CalibrationClasses as Cls
-import CalibrationSettings as P
+import CalibrationSettings as CalibSets
 import SimPy.FigureSupport as Fig
+import SimPy.SamplePathClasses as Path
 
 
 # initialize a calibrated model
 calibrated_model = Cls.CalibratedModel(csv_file_name='CalibrationResults.csv')
 # simulate the calibrated model
-calibrated_model.simulate(num_of_simulated_cohorts=P.NUM_SIM_COHORTS,
-                          cohort_size=P.SIM_POP_SIZE,
-                          time_steps=P.TIME_STEPS)
+calibrated_model.simulate(num_of_simulated_cohorts=CalibSets.NUM_SIM_COHORTS,
+                          cohort_size=CalibSets.SIM_POP_SIZE,
+                          time_steps=CalibSets.TIME_STEPS)
+
+# plot the sample paths
+Path.graph_sample_paths(
+    sample_paths=calibrated_model.multiCohorts.multiCohortOutcomes.survivalCurves,
+    title='Survival Curves',
+    x_label='Time-Step (Year)',
+    y_label='Number Survived',
+    transparency=0.5)
 
 # plot the histogram of mean survival time
 Fig.graph_histogram(
@@ -17,8 +26,8 @@ Fig.graph_histogram(
     x_label='Mean Survival Time (Year)',
     y_label='Count',
     bin_width=0.25,
-    x_range=[7, 13])
+    x_range=[2.5, 21.5])
 
 # report mean and projection interval
-print('Mean survival time and {:.{prec}%} projection interval:'.format(1 - P.ALPHA, prec=0),
-      calibrated_model.get_mean_survival_time_proj_interval(alpha=P.ALPHA))
+print('Mean survival time and {:.{prec}%} projection interval:'.format(1 - CalibSets.ALPHA, prec=0),
+      calibrated_model.get_mean_survival_time_proj_interval(alpha=CalibSets.ALPHA))
---
+++ RunUncalibratedModel.py
@@ -0,0 +1,46 @@
+import CalibrationSettings as CalibSets
+import numpy as np
+import MultiSurvivalModelClasses as Cls
+import SimPy.FigureSupport as Fig
+import SimPy.SamplePathClasses as Path
+
+# find values of mortality probability at which the posterior should be evaluated
+mortality_samples = np.random.uniform(
+    low=CalibSets.POST_L,
+    high=CalibSets.POST_U,
+    size=CalibSets.POST_N)
+
+# create multiple cohorts
+multiCohort = Cls.MultiCohort(
+    ids=range(CalibSets.POST_N),
+    pop_sizes=[CalibSets.SIM_POP_SIZE] * CalibSets.NUM_SIM_COHORTS,
+    mortality_probs=mortality_samples  # [p, p, ....]
+)
+
+# simulate all cohorts
+multiCohort.simulate(CalibSets.TIME_STEPS)
+
+# plot the sample paths
+Path.graph_sample_paths(
+    sample_paths=multiCohort.multiCohortOutcomes.survivalCurves,
+    title='Survival Curves',
+    x_label='Time-Step (Year)',
+    y_label='Number Survived',
+    transparency=0.5)
+
+# plot the histogram of average survival time
+Fig.graph_histogram(
+    data=multiCohort.multiCohortOutcomes.meanSurvivalTimes,
+    title='Histogram of Mean Survival Time',
+    x_label='Mean Survival Time (Year)',
+    y_label='Count',
+    x_range=[2.5, 21.5],
+    bin_width=0.5)
+
+# print projected mean survival time (years)
+print('Projected mean survival time (years)',
+      multiCohort.multiCohortOutcomes.statMeanSurvivalTime.get_mean())
+
+# print projection interval
+print('95% projection (prediction, percentile, or uncertainty) interval of average survival time (years)',
+      multiCohort.multiCohortOutcomes.statMeanSurvivalTime.get_PI(alpha=0.05))
