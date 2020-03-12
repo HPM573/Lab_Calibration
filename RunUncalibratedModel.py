4e5b68cd1a734a6c3dfeefb992aa4ebd9653eb0f
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-03-11 22:05:24 -0400

    updated calib

--- RunUncalibratedModel.py
+++ RunUncalibratedModel.py
@@ -1,24 +1,25 @@
-import CalibrationSettings as CalibSets
+import CalibrationSettings as Sets
 import numpy as np
 import MultiSurvivalModelClasses as Cls
 import SimPy.Plots.Histogram as Hist
 import SimPy.Plots.SamplePaths as Path
 
 # find values of mortality probability at which the posterior should be evaluated
+np.random.seed(1)
 mortality_samples = np.random.uniform(
-    low=CalibSets.POST_L,
-    high=CalibSets.POST_U,
-    size=CalibSets.POST_N)
+    low=Sets.POST_L,
+    high=Sets.POST_U,
+    size=Sets.POST_N)
 
 # create multiple cohorts
 multiCohort = Cls.MultiCohort(
-    ids=range(CalibSets.POST_N),
-    pop_sizes=[CalibSets.SIM_POP_SIZE] * CalibSets.NUM_SIM_COHORTS,
-    mortality_probs=mortality_samples  # [p, p, ....]
+    ids=range(Sets.POST_N),
+    pop_sizes=[Sets.SIM_POP_SIZE] * Sets.NUM_SIM_COHORTS,
+    mortality_probs=mortality_samples  # [p1, p2, ....]
 )
 
 # simulate all cohorts
-multiCohort.simulate(CalibSets.TIME_STEPS)
+multiCohort.simulate(Sets.TIME_STEPS)
 
 # plot the sample paths
 Path.plot_sample_paths(
@@ -36,6 +37,14 @@ Hist.plot_histogram(
     y_label='Count',
     x_range=[2.5, 21.5],
     bin_width=0.5)
+
+# create the histogram of the mortality probabilities
+Hist.plot_histogram(
+    data=mortality_samples,
+    title='Histogram of Mortality Probabilities',
+    x_label='Mortality Probability',
+    y_label='Counts',
+    x_range=[Sets.POST_L, Sets.POST_U])
 
 # print projected mean survival time (years)
 print('Projected mean survival time (years)',
